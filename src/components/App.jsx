5b6cb5d0d6de4f84c96fa4ab2a407b6a275f6990
unknown <yummyfunnybunny13@gmail.com>
2020-03-08 22:27:57 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+// import styles from "./public.styles.css";
+
+function App() {
+    return(
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+};
+
+export default App;
