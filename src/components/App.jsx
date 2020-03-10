736e689ae6818d927a7d81745de7afa2b3c5be59
unknown <yummyfunnybunny13@gmail.com>
2020-03-09 21:57:03 -0400

    added mapping function from notes.js dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
 // import styles from "./public.styles.css";
+
+
 
 function App() {
     return(
         <div>
             <Header />
-            <Note />
+                {notes.map(noteItem => (
+                    <Note
+                        key={noteItem.key}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                    />
+                ))}
             <Footer />
         </div>
     )
